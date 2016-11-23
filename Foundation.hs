a33ba62486674218c60bcb705e89561f6b9ce8a3
Felipe Queiroz <felipe.squeiroz@hotmail.com>
2016-11-23 14:07:46 +0000

    Main.hs e Foundation.hs criados

---
+++ Foundation.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilies, QuasiQuotes,
+             TemplateHaskell, GADTs, FlexibleContexts,
+             MultiParamTypeClasses, DeriveDataTypeable, EmptyDataDecls,
+             GeneralizedNewtypeDeriving, ViewPatterns, FlexibleInstances #-}
+module Foundation where
+
+import Yesod
+import Data.Text
+import Database.Persist.Postgresql
+    ( ConnectionPool, SqlBackend, runSqlPool)
+
+data App = App {connPool :: ConnectionPool }
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Cliente
+    nome     Text
+    endereco Text
+    telefone Text
+    
+|]
+
+mkYesodData "App" $(parseRoutesFile "routes")
+
+instance Yesod App
+
+instance YesodPersist App where
+   type YesodPersistBackend App = SqlBackend
+   runDB f = do
+       master <- getYesod
+       let pool = connPool master
+       runSqlPool f pool
+
+type Form a = Html -> MForm Handler (FormResult a, Widget)
+
+instance RenderMessage App FormMessage where
+    renderMessage _ _ = defaultFormMessage
---
+++ Main.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes,
+             TemplateHaskell #-}
+ 
+module Main where
+import Application
+import Yesod
+import Yesod.Static
+import Foundation
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Applicative
+import Data.Text
+import Database.Persist.Postgresql
+
+connStr = "dbname=d7f2o1s09j6k37 host=ec2-107-22-238-96.compute-1.amazonaws.com user=euiflrvvoqtphn password=cneAb8rnPXLKltdvfac-6G6dLU port=5432"
+
+main::IO()
+main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do 
+       runSqlPersistMPool (runMigration migrateAll) pool 
+       t@(Static settings) <- static "static"
+       warp 8080 (Sitio t pool)
